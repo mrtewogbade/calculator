3b9c2905a8f5a1b716720f0f06cbb19873c074a1
mrtewogbade <johndharey@gmail.com>
2023-06-16 18:38:49 +0000

    adding functionality

---
+++ script.js
@@ -0,0 +1,159 @@
+let currentNum = '';
+let previousNum = '';
+let operator = '';
+
+const currentDisplayNumber = document.querySelector('.currentNumber');
+
+const previousDisplayNumber = document.querySelector('.previousNumber');
+
+window.addEventListener('keydown', handleKeyPress);
+
+// const btn = document.querySelectorAll('button');
+
+const equal = document.querySelector('.equals-button');
+equal.addEventListener('click', () => {
+    if (currentNum != '' && previousNum != ''){
+        performCalculation()
+    }
+})
+
+const decimal = document.querySelector('.decimal-button');
+decimal.addEventListener('click', () => {
+    addDecimal();
+})
+
+const clear = document.querySelector('.clear-button');
+clear.addEventListener('click', clearCalculator)
+
+const numberButtons = document.querySelectorAll('.digit-button');
+
+const operators = document.querySelectorAll('.operator');
+
+numberButtons.forEach((btn) => {
+    btn.addEventListener('click', (e) =>{
+        handleNumber(e.target.textContent)
+    })
+});
+
+function handleNumber(number) {
+
+    if (previousNum !== '' && currentNum !== '' && operator === '') {
+        previousNum = '';
+        currentDisplayNumber.textContent = currentNum;
+    }
+
+    if (currentNum.length <= 11) {
+        currentNum += number;
+        currentDisplayNumber.textContent = currentNum;
+    }
+}
+
+operators.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+        handleOperator(e.target.textContent);
+    });
+});
+
+
+
+
+function handleOperator(op){
+    if (previousNum === "") {
+        previousNum = currentNum;
+        operatorCheck(op);
+    }else if (currentNum === "") {
+        operatorCheck(op)
+    }else{
+        performCalculation();
+        operator = op;
+        currentDisplayNumber.textContent = '0';
+        previousDisplayNumber.textContent = previousNum + ' ' + operator;
+    }
+}
+
+function operatorCheck(text) {
+    operator = text;
+    previousDisplayNumber.textContent = previousNum + ' ' + operator;
+    currentDisplayNumber.textContent = '0';
+    currentNum = '';
+}
+
+function performCalculation() {
+    previousNum = Number(previousNum)
+    currentNum = Number(currentNum)
+
+    if (operator === "+") {
+        previousNum += currentNum
+    }else if (operator === "-") {
+        previousNum -= currentNum;
+    } else if (operator === "x") {
+        previousNum *= currentNum;
+    }
+    else if (operator === "/") {
+        if (currentNum <= 0) {
+            previousNum = 'Error';
+            displayResults()
+            return;
+        }
+        previousNum /= currentNum;
+    }
+    previousNum = roundNumber(previousNum);
+    previousNum = previousNum.toString();
+    displayResults();
+}
+
+function roundNumber(num) {
+    return Math.round(num * 100000) / 100000
+}
+
+function displayResults() {
+
+    if (previousNum.length <= 11) {
+        currentDisplayNumber.textContent = previousNum;
+    } else{
+        currentDisplayNumber.textContent = previousNum.slice(0, 11) + "..."
+    }
+
+    previousDisplayNumber.textContent = '';
+    operator = '';
+    currentNum = '';
+}
+
+function clearCalculator() {
+    currentNum = ''
+    previousNum = ''
+    operator = ''
+    currentDisplayNumber.textContent = '0';
+    previousDisplayNumber.textContent = '0'
+}
+
+function addDecimal() {
+    if (!currentNum.includes('.')){
+        currentNum += "."
+        currentDisplayNumber.textContent = currentNum;
+    }
+}
+
+function handleKeyPress(e) {
+    e.preventDefault()
+    if (e.key >= 0 && e.key <= 9){
+        handleNumber(e.key)
+    }
+    if(e.key === 'Enter' || (e.key === "=" && currentNum!= '' && previousNum))
+    {
+        performCalculation();
+    }
+    if (e.key === '+' || e.key === '-' || e.key === '/'){
+        handleOperator(e.key);
+    }
+    if (e.key === "*"){
+        handleOperator('x');
+    }
+    if (e.key === '.'){
+        addDecimal();
+    }
+}
+
+// equal.addEventListener('click', () => {
+
+// })
